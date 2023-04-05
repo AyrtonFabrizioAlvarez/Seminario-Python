b64f2c085d5a0b62bcb036d590c358e499d4dcbc
AyrtonFabrizio <ayrtonfabrizioalvarez@gmail.com>
2023-04-04 23:22:19 -0300

    practica2 ej10

--- Practica 2/ej10.py
+++ Practica 2/ej10.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 names = ''' 'Agustin', 'Alan', 'Andrés', 'Ariadna', 'Bautista', 'CAROLINA', 'CESAR', 'David',
 'Diego', 'Dolores', 'DYLAN', 'ELIANA', 'Emanuel', 'Fabián', 'Facundo', 'Francsica', 
 'FEDERICO', 'Fernanda', 'GONZALO', 'Gregorio', 'Ignacio', 'Jonathan', 'Joaquina', 'Jorge',
@@ -11,20 +13,60 @@ notes_2 = [30, 95, 28, 84, 84, 43, 66, 51, 4, 11, 58, 10, 13, 34, 96, 71, 86, 37
            87, 14, 14, 49, 27, 55, 69, 77, 59, 57, 40, 96, 24, 30, 73, 95, 19, 47, 15, 31,
            39, 15, 74, 33, 57, 10]
 
-namesFixed = []
-def fixNames():
-    global namesFixed
-    namesFixed = names.replace('\n','').split(',')
+def generateZip(names, notes1, notes2):
+    ''' Esta funcion retorna una lista de tuplas que fue un zip de 3 archivos (names <string>, notes1 <list>, notes2<list>)'''
+    return list(zip(names.replace('\n','').replace(' ', '').replace("'", '').split(','),notes1, notes2))
 
-unificated = []
-def unificate():
-    global unificated
-    unificated = list(zip(namesFixed,notes_1, notes_2))
+def promNotes(L):
+    '''Esta funcion retorna un diccionario donde key = "nombre", value = "promedio"'''
+    promStud = {}
+    totalCourse = 0
+    for tup in L:
+        total = tup[1] + tup[2]
+        prom = total / 2
+        promStud[tup[0]] = prom
+    return promStud
+
+def promGlobal(L):
+    '''Esta funcion retorna un double con el promedio general de la clase'''
+    totalCourse = 0
+    for tup in L:
+        totalCourse += tup[1] + tup[2]
+    return round((totalCourse / len(L)), 2)
+
+def highestProm(D):
+    '''Esta funcion devuelve 2 valores (nombreEstudiante, promedio)'''
+    max = -1
+    for note in D:
+        if D[note]> max:
+            max = D[note]
+            maxName = note
+    return maxName, max
+
+def lowestNote(L):
+    '''Esta funcion devuelve 2 valores (nombreEstudiante, nota)'''
+    min = 999
+    for tup in L:
+        if tup[1] < min:
+            min = tup[1]
+            minName = tup[0]
+        if tup[2] < min:
+            min = tup[2]
+            minName = tup[0]
+    return minName, min
 
 
-fixNames()
-unificate()
+zipedList = generateZip(names, notes_1, notes_2)
+#print(zipedList)
 
+promStuds = promNotes(zipedList)
+print(promStuds)
 
-for element in unificated:
-    print(element)
+promClass = promGlobal(zipedList)
+#print(promClass)
+
+highPromStud, prom = highestProm(promStuds)
+#print(highPromStud, prom)
+
+lowerNoteStud, note = lowestNote(zipedList)
+#print(lowerNoteStud, note)
